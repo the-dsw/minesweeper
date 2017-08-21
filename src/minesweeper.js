956e4736fac97989a383eaa253b8deefadf3d975
the-dsw <fmod@the-dsw.com>
2017-08-21 15:55:37 +0200

    init commit

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+
+const blackLine = '  |   |  '
+console.log('This is what an empty board would look like:')
+
+console.log(blackLine)
+console.log(blackLine)
+console.log(blackLine)
+
+const guessLine = '1 |   |  '
+const bombLine = '  | B |  '
+console.log('This is what a board with a guess and a bomb on it would look like:')
+
+console.log(guessLine)
+console.log(bombLine)
+console.log(blackLine)

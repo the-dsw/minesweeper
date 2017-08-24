297a4440a738559f86982d10d3ed779bf99a0d94
the-dsw <fmod@the-dsw.com>
2017-08-24 13:04:33 +0200

    Modules separated a final project

--- src/board.js
+++ src/board.js
@@ -1,24 +1,4 @@
-class Game {
-    constructor(numberOfRows, numberOfColumns, numberOfBombs) {
-        this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs);
-    }
-
-    playMove(rowIndex, columnIndex) {
-        this._board.flipTile(rowIndex, columnIndex);
-        if (this._board.playerBoard[rowIndex][columnIndex] === 'B') {
-            console.log('Game Over! Final Board:');
-            this._board.print();
-        } 
-        if(this._board.playerBoard[rowIndex][columnIndex] !== 'B') {
-            console.log('congratulates the user on winning');
-        } else {
-            console.log('Current Board:');
-            this._board.print();
-        }
-    }
-}
-
-class Board {
+export class Board {
     constructor(numberOfRows, numberOfColumns, numberOfBombs) {
         this._numberOfBombs = numberOfBombs;
         this._numberOfTiles = numberOfRows * numberOfColumns;
@@ -109,15 +89,3 @@ class Board {
         return board;
     };
 }
-
-
-const g = new Game(3, 3, 3);
-g.playMove(1, 0); 
-  
- 
-  
-  
-  
- 
-  
-  
